c7baa75903f93e65d7b547d72847425da7566127
Ryan McCabe <ryan.s.mccabe@gmail.com>
2017-02-24 13:22:23 -0600

    created a verifyState function that is to work on blur.  The function is currently running into an infinite loop.  It still needs more revising

--- stateinformation.js
+++ stateinformation.js
@@ -12,7 +12,11 @@ $(document).ready(function () {
  * send inputs to other functions for validation or displaying for the user
  */
 function pageHandler(event) {
+    //make sure page doesn't reload and reset errors on page every attempt
     event.preventDefault();
+    resetErrors();
+
+    //validate if the state provided is correct and use act accordingly
 
 
 }
@@ -22,11 +26,34 @@ function pageHandler(event) {
  * else provide an error for the user
  */
 function verifyState () {
-
+    event.preventDefault();
+    var userState = $('[name="state"]').val.toUpperCase();
+    console.log(userState);
+    var validState = false;
+    var stateAbbreviations = ["DC","AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA",
+        "ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK",
+        "OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"];
+    $.each(stateAbbreviations), function(state) {
+        if (state == userState){
+            validState = true;
+            $('[type="submit"]').prop("disabled", false)
+        } else {
+            $("#error")
+                .attr("hidden", false)
+                .html("This is not a valid state");
+        }
+    };
+    return validState;
 }
 /**
  * Will find take the state data and create modify the index page to create a bootstrap table
  */
 function displayData (){
 
+}
+
+function resetErrors (){
+    $("#error")
+        .attr("hidden", true)
+        .html("");
 }
